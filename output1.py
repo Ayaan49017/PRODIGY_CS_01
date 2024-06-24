06e0b338f88f39714c0d352ea9e761d734339295
Ayaan49017 <2200049017ece@gmail.com>
2024-06-24 21:34:36 +0530

    Add files via upload

---
+++ output1.py
@@ -0,0 +1,5 @@
+PS C:\Users\jamee> & C:/Users/jamee/AppData/Local/Programs/Python/Python310/python.exe c:/Users/jamee/OneDrive/Desktop/ProdigyInfotech/Task-1/Untitled-1.py
+Would you like to encrypt or decrypt the message? (e/d): e
+Enter the message: hello world 
+Enter the shift value: 5
+Encrypted message: mjqqt btwqi 
